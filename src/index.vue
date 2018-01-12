8aeeb2a029853c56dbe230a2669b98dcc6463a90
lewei li <lewei.li@qq.com>
2018-01-13 00:12:13 +0800

    test change

--- src/index.vue
+++ src/index.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="wrapper" @click="update">
-    <image :src="logoUrl" class="logo"></image>
-    <text class="title">Hello {{target}}</text>
+    <!-- <image :src="logoUrl" class="logo"></image> -->
+    <div class="container" >
+      <text class="title-h5">h5</text>
+
+      <div class="cell">
+         <image class="thumb" src="http://t.cn/RGE3AJt"></image>  
+      </div>
+    </div>
+    <text class="title">Start {{target}}</text>
     <text class="desc">Now, let's use vue to build your weex app.</text>
   </div>
 </template>
 
 <style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+  .wrapper { align-items: center; margin-top: 20px; }
+  .title { padding-top:40px; padding-bottom: 40px;font-size: 48px; }
   .logo { width: 360px; height: 156px; }
   .desc { padding-top: 20px; color:#888; font-size: 24px;}
+  .cell{margin-top:10px; margin-left:10px; flex-direction: row; }
+  .thumb {width: 200px; height: 200px; display: block; }
+  .title-h5 {text-align: center ; flex: 1; color: grey; font-size: 50px; display: block;}
+  .container {
+
+/*    width: auto;
+    height: 200px;*/
+  }
 </style>
 
 <script>
